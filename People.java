3268acfcb468cb48bb8037759555b2b2aa936467
ziszed29 <32400289+karthikkolathumani@users.noreply.github.com>
2017-11-10 03:13:20 -0800

    Add files via upload
    This File Requires ObjectStack

---
+++ People.java
@@ -0,0 +1,18 @@
+package DataStructure;
+
+public class People {
+	String name;
+	String age;
+	public People(String name,String age){
+		this.name=name;
+		this.age=age;
+		
+	}
+
+	
+	public String toString(){
+		return "Name: " + this.name + " Rollnumber: " + this.age;
+	}
+	
+	
+}
