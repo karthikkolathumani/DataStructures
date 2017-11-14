774255720a72f4439e5ec6cbbec37caf60b142d4
ziszed29 <32400289+karthikkolathumani@users.noreply.github.com>
2017-11-14 14:45:07 -0800

    InsertMiddle Method

--- LinkedLists.java
+++ LinkedLists.java
@@ -65,13 +65,76 @@ public class LinkedLists {
 	}
 	}
 	
+	public boolean insertLast(int element){
+		Node n;
+		n = head;
+		while(n.address != null){
+			n = n.address;
+			
+		}
+		Node last = new Node();
+		last.address = null;
+		last.value = element;
+		n.address = last;
+		
+		
+		return true;
+	}
+	
+	
+	public boolean insertMiddle(int element, int search){
+		Node sea, sea1;
+		
+		if(head.value == search ){
+			
+			Node newNode = new Node();
+			
+			newNode.value = element;
+			newNode.address = head.address;
+			head.address = newNode;
+			return true;
+			
+		}
+		else{
+		sea = head;
+		sea1 = head.address;
+		while(true){
+			if(sea1 == null || sea1.value == search){
+				break;
+			}
+			else
+			{
+				sea = sea1;
+				sea1 = sea1.address;
+				
+			}
+			
+		}
+		if(sea1!=null){
+			Node newNode = new Node();
+			
+			newNode.value = element;
+			newNode.address = sea1.address;
+			sea1.address = newNode;
+			return true;
+		}
+		else{
+		
+		return false;
+		}
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		LinkedLists ls = new LinkedLists(2);
 		ls.inserRec(12);
 		ls.inserRec(23);
-		ls.print();
-		System.out.println(ls.deleteNode(2));
+		//ls.print();
+		//System.out.println(ls.deleteNode(2));
+		
+		ls.insertLast(100);
+		ls.insertMiddle(32, 12);
 		ls.print();
 	
 	}
